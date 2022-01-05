12801a395f46983fb4c8824f898988a31da0fa58
Satyam <ss9125rpvv@gmail.com>
2022-01-05 23:33:40 +0530

    functionality finished

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
-let p1Btn = document.querySelector('.player1-fire');
-let p2Btn = document.querySelector('.player2-fire');
+const p1Btn = document.querySelector('.player1-fire');
+const p2Btn = document.querySelector('.player2-fire');
+const start = document.querySelector('.start');
 
-let p1Health = document.querySelector('.player1-health');
-let p2Health = document.querySelector('.player2-health');
+const p1Health = document.querySelector('.player1-health');
+const p2Health = document.querySelector('.player2-health');
 
-let player1Score = document.querySelector(".score-1");
-let player2Score = document.querySelector(".score-2");
+const p1Power = document.querySelector('.p1-power');
+const p2Power = document.querySelector('.p2-power');
 
-// let gameOver = document.querySelector('.game-over')
-let p1Score = 100;
+const player1Score = document.querySelector(".score-1");
+const player2Score = document.querySelector(".score-2");
+
+// const gameOver = document.querySelector('.game-over')
 let p2Score = 100;
+let p1Score = 100;
 
 let p1Won = 0;
 let p2Won = 0;
 
+let generateRandomPower = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+start.addEventListener("click", () => {
+  const players = document.querySelectorAll('.player');
+  players.forEach((player) => player.classList.remove('game-hidden'));
+})
+
+function restartGame() {
+  p1Won = 0;
+  player1Score.innerHTML = p1Won
+  p2Won = 0;
+  player2Score.innerHTML = p1Won
+  p1Health.value = 100;
+  p2Health.value = 100;
+  document.querySelectorAll('.game-over').forEach((item) => item.remove());
+  p1Btn.disabled = false;
+  p2Btn.disabled = false;
+}
+
 function gameOver(winner) {
-  let winnerText = document.createElement("div");
+  const winnerText = document.createElement("div");
   winnerText.classList.add("game-over");
-  let text = document.createTextNode(winner);
+  const text = document.createTextNode(winner + "\nGame Over");
   winnerText.appendChild(text);
   document.body.appendChild(winnerText);
 
-  let gameOverAnimation = document.createElement("div");
-  gameOverAnimation.classList.add("ball-2");
-  document.body.appendChild(gameOverAnimation);
+  p1Power.innerHTML = '';
+  p2Power.innerHTML = '';
+
+  p1Btn.disabled = true
+  p2Btn.disabled = true
 }
 
 // for player one 
-p1Btn.addEventListener('click', function() {
-    let ball = document.createElement("div");
-    ball.classList.add("ball-1");
-    ball.classList.add("ball-animate");
-    document.body.appendChild(ball);
+p1Btn.addEventListener('click', () => {
+    const bullet = document.createElement("div");
+    bullet.classList.add("bullet-1");
+    bullet.classList.add("bullet-animate");
+    document.body.appendChild(bullet);
 
     setTimeout(function() {
-      ball.remove();
-      p2Health.value -= 5;
+      bullet.remove();
+      let reduceHealthBy = generateRandomPower(5, 0);
+      p2Health.value -= reduceHealthBy;
+      p2Power.innerHTML = `-${reduceHealthBy}`;
       
       if(p2Health.value === 0){
-        p1Won += 1;
-        p2Health.value = 100;
-        player1Score.innerHTML = p1Won;
+        if (p1Won < 3) {
+          p1Won += 1;
+          p2Health.value = 100;
+          player1Score.innerHTML = p1Won;
+        }  
       }
 
       if(p1Won === 3){
@@ -49,36 +78,35 @@ p1Btn.addEventListener('click', function() {
       else if(p2Won === 3) {
         gameOver("player-2 Won");
       }
-    }, 1000)
-})
+    }, 1000);
+});
 
 // for player two
-p2Btn.addEventListener('click', function() {
-    let ball = document.createElement("div");
-    ball.classList.add("ball-2");
-    // ball.classList.add("ball-animate");
-    document.body.appendChild(ball);
+p2Btn.addEventListener('click', () => {
+    const bullet = document.createElement("div");
+    bullet.classList.add("bullet-2");
+    document.body.appendChild(bullet);
 
     setTimeout(function() {
-      ball.remove();
-      p1Health.value -= 5;
-
-
+      bullet.remove();
+      let reduceHealthBy = generateRandomPower(5, 0);
+      p1Health.value -= reduceHealthBy;
+      p1Power.innerHTML = `-${reduceHealthBy}`;
+      
       if(p1Health.value === 0){
-        p2Won += 1;
-        p1Health.value = 100;
-        player2Score.innerHTML = p2Won;
+        if(p2Won < 3) {
+          p2Won += 1;
+          p1Health.value = 100;
+          player2Score.innerHTML = p2Won;
+        }
       }
 
-      if(p1Won === 3){
-        gameOver("Player-1 Won");
-      }
-      else if(p2Won === 3) {
+      if(p2Won === 3){
         gameOver("Player-2 Won");
       }
+      else if(p1Won === 3) {
+        gameOver("Player-1 Won");
+      }
       
-    }, 1000)
-    
-    
-
-})
+    }, 1000);
+});
